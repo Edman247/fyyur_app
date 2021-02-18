b2791e1e322803812c9bf073dc61fbc4520c5a58
Edmanuel247 <redmanuel247@gmail.com>
2021-02-18 00:52:05 -0500

    implemented artist/search functionality

--- starter_code/app.py
+++ starter_code/app.py
@@ -265,18 +265,23 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
-  response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
-  }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+    # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
+    # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
+    # search for "band" should return "The Wild Sax Band".
+    search_term=(request.form.get('search_term'))
+    search_term = "%{}%".format(search_term)
+    print(search_term)
+    artists = Artist.query.filter(Artist.name.ilike(search_term)).all()
+    for artist in artists:
+        print(artist)
+    response={
+        'count': len(artists),
+        'data': [{
+          'id': artist.id,
+          'name': artist.name,
+        } for artist in artists]
+    }
+    return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
